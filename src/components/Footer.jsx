919e58c95cbdb0f11b5b34068fb12708ded4cc3a
Kashish singh <singhkashish14203@gmail.com>
2025-05-26 10:30:09 +0530

    upadted

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer className="w-full flex justify-between items-center px-6 py-4 mt-40 text-xl font-bold text-black">
-      <span>made by @codrops</span>
-      <span>all demos</span>
+      <span>made by <span className='relative cursor-pointer hover-underline'>@codrops</span></span>
+      <span className='relative cursor-pointer hover-underline'>all demos</span>
     </footer>
   );
 };
